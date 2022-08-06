9b83ea87d6d81aaf423a1719ab13518a61398023
LeonardoRenoldi <95914933+LeonardoRenoldi@users.noreply.github.com>
2022-08-05 23:46:33 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+export default function App() {
+  return (
+    <div className="App">
+      <Personagens />
+    </div>
+  );
+}
+
+const Personagens = () => {
+  const personagens = [
+    "Jill Valentine",
+    "Claire Redfield",
+    "Ada Wong",
+    "Chris Redfield",
+    "Leon S. Kennedy",
+    "Albert Wesker"
+  ];
+  return (
+    <div>
+      <h3>Personagens de Resident Evil</h3>
+      <ul>
+        {personagens
+          .filter((personagens) => personagens.includes("Redfield"))
+          .map((personagem) => (
+            <li>{personagem}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
